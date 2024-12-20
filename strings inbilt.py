7ba4906df96dfae76c9bbb6dd18733ad65164695
krishna-0904 <krishnaprabhas601@gmail.com>
2024-12-20 09:00:37 +0530

    update

--- strings inbilt.py
+++ strings inbilt.py
@@ -1,7 +1,19 @@
 # print("hello😘".upper())
 
-print("A","ord--->",ord("A"))
-print("a","ord--->",ord("a"))
+# print("A","ord--->",ord("A"))
+# print("a","ord--->",ord("a"))
 
-print(65,"chr--->",chr(65))
-print(97,"chr--->",chr(97))
+# print(65,"chr--->",chr(65))
+# print(97,"chr--->",chr(97))
+
+# val="ABC"
+# for x in val:
+#     ord_val=ord(x)
+#     conv_chr=ord_val+32
+#     print(chr(conv_chr))
+
+val="abc"
+for x in val:
+    ord_val=ord(x)
+    conv_chr=ord_val-32
+    print(chr(conv_chr))
